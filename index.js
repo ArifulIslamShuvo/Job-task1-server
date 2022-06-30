44ef9b122d99d31b05557ad607dc66995c15b51f
ArifulIslamShuvo <arifulislams063@gmail.com>
2022-06-30 23:59:47 +0100

    create a post api for daily task

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -11,6 +11,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uwy5t.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const dailyTaskCollection = client.db('dailyTask').collection('task');
+
+        app.post('/task', async(req, res) =>{
+            const newTask = req.body;
+            const result = await dailyTaskCollection.insertOne(newTask);
+            res.send(result);
+        });
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) =>{
